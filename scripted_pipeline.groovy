ff1ec99d9c878393aa7b0f86a68dec1ff844d1c5
Ragavendira1 <rragavendira@gmail.com>
2020-05-12 14:27:20 +0530

    Create scripted_pipeline.groovy

---
+++ scripted_pipeline.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+// Scripted Pipeline
+node {
+
+	stage('Build') {
+		echo 'Building....'
+	}
+
+	stage('Test') {
+		echo 'Building....'
+	}
+
+	stage('Deploy') {
+		echo 'Deploying....'
+	}
+}
+© 2020 GitHub, Inc.
